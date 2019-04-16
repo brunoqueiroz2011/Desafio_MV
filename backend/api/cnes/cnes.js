b5abbd97fb12d86f5a3cae4d61735c50d1e97a81
Bruno Gonçalves de Queiroz <bruno.queiroz2011@gmail.com>
2019-04-16 15:48:49 -0300

    Inclusão das mensagens de validações

--- backend/api/cnes/cnes.js
+++ backend/api/cnes/cnes.js
@@ -2,17 +2,17 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const cnesSchema = new mongoose.Schema({
-    co_cnes:{type: Number, require: true},
-    co_ibge:{type: String, require: true},
-    no_fantasia:{type: String, require: true},
-    ds_tipo_unidade:{type: String, require: true},
-    tp_gestao:{type: String, require: true},
-    no_logradouro:{type: String, require: true},
-    nu_endereco:{type: String, require: true},
-    no_bairro:{type: String, require: true},
-    co_cep:{type: String, require: true},
-    uf:{type: String, require: true},
-    municipio:{type: String, require: true},
+    co_cnes:{type: Number, require: [true, "Informe o valor do código do CNES"]},
+    co_ibge:{type: String, require: [true, "Informe o valor do código do IBGE"]},
+    no_fantasia:{type: String, require: [true, "Informe o valor do nome fantasia"]},
+    ds_tipo_unidade:{type: String, require: [true, "Informe o valor do tipo da unidade"]},
+    tp_gestao:{type: String, require: [true, "Informe o valor do tipo da gestão"], uppercase: true},
+    no_logradouro:{type: String, require: [true, "Informe o valor do nome do logradouro"]},
+    nu_endereco:{type: String, require: [true, "Informe o valor do nome do endereço"]},
+    no_bairro:{type: String, require: [true, "Informe o valor do nome do bairro"]},
+    co_cep:{type: String, require: [true, "Informe o valor do código do CEP"]},
+    uf:{type: String, require: [true, "Informe o valor do UF"], uppercase: true},
+    municipio:{type: String, require: [true, "Informe o valor do nome do Municipio"]},
     nu_telefone :{type: String, require: false}   
 })
 
