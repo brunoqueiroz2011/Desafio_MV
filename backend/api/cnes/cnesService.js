c4f1dbcc4e7c9a7ffddc858b996e4ee4817df38d
Bruno Gonçalves de Queiroz <bruno.queiroz2011@gmail.com>
2019-04-16 15:50:05 -0300

    Inclusão do método/rota count para totalizar a quantidade de resgitro no banco

--- backend/api/cnes/cnesService.js
+++ backend/api/cnes/cnesService.js
@@ -1,5 +1,16 @@
 const Cnes = require('./cnes')
 
-Cnes.methods(['get','post'])
+Cnes.methods(['get','post','put','delete'])
+Cnes.updateOptions({new: true, runValidators: true})
+
+Cnes.route('count', function(req, res, next){
+    Cnes.count(function(error, value){
+        if(error){
+            res.status(500).json({errors: [error]})
+        }else{
+            res.json({value})
+        }
+    })
+})
 
 module.exports = Cnes
