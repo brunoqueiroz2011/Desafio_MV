cd6cbd3fa7ff0bea04c67f7f674866e8fac1f18c
Bruno Gonçalves de Queiroz <bruno.queiroz2011@gmail.com>
2019-04-18 15:52:40 -0300

    Ajustes das rotas: lista, tipos, estados para seus devidos componentes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component';
+import { CnestiposComponent } from './cnestipos/cnestypes.component';
+import { CnesstateComponent } from './cnesstate/cnesstate.component';
+import { CneslistComponent } from './cneslist/cneslist.component';
+
 
 export const ROUTES: Routes = [
     {path:'', component: HomeComponent},
     {path:'home', component: HomeComponent} ,
-    {path:'cneslista', component: HomeComponent},
-    {path:'cnestipos', component: HomeComponent},
-    {path:'cnesestados', component: HomeComponent},
+    {path:'cneslista', component: CneslistComponent},
+    {path:'cnestipos', component: CnestiposComponent},
+    {path:'cnesestados', component: CnesstateComponent},
     {path:'cnesrelatorios', component: HomeComponent}
 ]
