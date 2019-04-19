8e31931de1d7b09e697233f6c6bd404974e7635c
Bruno Gonçalves de Queiroz <bruno.queiroz2011@gmail.com>
2019-04-18 21:40:12 -0300

    Ajuste na rota de relatorio, direcionando para o componente de Relatorios

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { CnestiposComponent } from './cnestipos/cnestypes.component';
 import { CnesstateComponent } from './cnesstate/cnesstate.component';
 import { CneslistComponent } from './cneslist/cneslist.component';
+import { CnesreportsComponent } from './cnesreports/cnesreports.component';
 
 
 export const ROUTES: Routes = [
@@ -11,5 +12,5 @@ export const ROUTES: Routes = [
     {path:'cneslista', component: CneslistComponent},
     {path:'cnestipos', component: CnestiposComponent},
     {path:'cnesestados', component: CnesstateComponent},
-    {path:'cnesrelatorios', component: HomeComponent}
+    {path:'cnesrelatorios', component: CnesreportsComponent}
 ]
