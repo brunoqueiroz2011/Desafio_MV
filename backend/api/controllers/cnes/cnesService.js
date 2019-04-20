478e7d07cad91003ef31c60449d8b1f025dbf334
Bruno Gonçalves de Queiroz <bruno.queiroz2011@gmail.com>
2019-04-20 02:11:44 -0300

    Ajuste na query, incluindo o skip que ignora uma determinada quantidade de registros

--- backend/api/controllers/cnes/cnesService.js
+++ backend/api/controllers/cnes/cnesService.js
@@ -3,7 +3,7 @@ const Cnes = require('../../models/cnes')
 Cnes.methods(['get','post','put','delete'])
 Cnes.updateOptions({new: true, runValidators: true})
 
-Cnes.route('count', function(req, res, next){
+Cnes.route('count', function(req, res, next){    
     Cnes.count(function(error, value){
         if(error){
             res.status(500).json({errors: [error]})
