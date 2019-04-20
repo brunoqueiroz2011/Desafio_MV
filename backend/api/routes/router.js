2ce92698762347baceb580a1541e2c3edd7bc098
Bruno Gonçalves de Queiroz <bruno.queiroz2011@gmail.com>
2019-04-20 02:13:15 -0300

    Ajuste no caminho dos arquivos

--- backend/api/routes/router.js
+++ backend/api/routes/router.js
@@ -1,15 +1,15 @@
 const express = require('express')
 
-module.exports = function(server){
-
+module.exports = function(server){    
     //API Routes
-    const router = express.Router()
+    const router = express.Router()    
     server.use('/api',router)
+    console.log(router)
 
-    const cnesService = require('../controllers/cnes/cnesService')
-    cnesService.register(router, '../../models/cnes')
+    const cnesService = require('../Controllers/cnes/cnesService')
+    cnesService.register(router,'/cnes')
 
-    const cnesSummaryService = require('../controllers/cnesSummary/cnesSummaryService')
+    const cnesSummaryService = require('../Controllers/cnesSummary/cnesSummaryService')
     router.route('/cnesgrouptbgestao').get(cnesSummaryService.getCountTypeManagement)
     router.route('/cnesgroupUF').get(cnesSummaryService.getCountStates)
     router.route('/cnesfirtsdocs/:limit').get(cnesSummaryService.getFirstDocs)
